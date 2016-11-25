8b189a44a729d42887a901bb0f69d8b9b187d550
MANISH-GIRI <manish.giri.me@gmail.com>
2016-11-25 03:01:02 -0500

    Add JavaScript, code for - - global variable for getting references to DOM elements - a function to determine the selected player, or select a default if unselected - when input for a player is clicked, disable other input

---
+++ Front-End/Projects/Ziplines/9-Tic-Tac-Toe/app.js
@@ -0,0 +1,68 @@
+/**
+ * Created by manishgiri on 11/24/16.
+ */
+$(document).ready(function () {
+
+    //global variables
+
+    //reference to board cells
+    var $tile1 = $("#one");
+    var $tile2 = $("#two");
+    var $tile3 = $("#three");
+    var $tile4 = $("#four");
+    var $tile5 = $("#five");
+    var $tile6 = $("#six");
+    var $tile7 = $("#seven");
+    var $tile8 = $("#eight");
+    var $tile9 = $("#nine");
+
+    //board cell contents
+    var tile1Content = $tile1.html();
+    var tile2Content = $tile2.html();
+    var tile3Content = $tile3.html();
+    var tile4Content = $tile4.html();
+    var tile5Content = $tile5.html();
+    var tile6Content = $tile6.html();
+    var tile7Content = $tile7.html();
+    var tile8Content = $tile8.html();
+    var tile9Content = $tile9.html();
+
+    //reference to x and o player box
+    var $xInp = $("#xInp");
+    var $oInp = $("#oInp");
+
+
+    //current player
+    var isXSelected = false;
+    var isPlayerSelected = false;
+
+    //function to determine current player based on input selection
+
+    function selectPlayer() {
+        $(".player-choice").click(function () {
+            console.log("inside select player");
+            if(this.id == 'xInp') {
+                $($oInp).prop("disabled", true);
+                console.log("Player X selected");
+                isXSelected = true;
+                isPlayerSelected = true;
+            }
+            else if(this.id == 'oInp') {
+                $($xInp).prop("disabled", true);
+                console.log("Player O selected");
+                isXSelected = false;
+                isPlayerSelected = true;
+            }
+
+        });
+
+        //if a player has not been selected, select X by default
+        if(!isPlayerSelected) {
+            isXSelected = true;
+            isPlayerSelected = true;
+        }
+    }
+
+    selectPlayer();
+
+})
