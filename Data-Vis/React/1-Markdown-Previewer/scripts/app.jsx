6d94c4dfee047df11eaf1a6c0902364af57da3f1
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-05 02:49:04 -0500

    Commit code submitted for FCC on codepen

--- Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
+++ Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
@@ -38,6 +38,8 @@ let UserInput = React.createClass({
         return {__html: newText}
     },
 
+
+
     generateMarkdown(event) {
         let messageRef = this.refs.userMessage;
         let messageVal = messageRef.value;
@@ -47,18 +49,20 @@ let UserInput = React.createClass({
             console.log(messageVal);
             //alert("Enter pressed");
             //call parent component with new value
+            this.props.onConvert(messageVal);
 
         }
     },
     render() {
-        let defaultText = this.props.default;
+        let defaultText = this.props.latest;
+        let defaultMarkdown = this.props.firstMDRender();
         return (
             <div className="display">
                 <div className="row">
                     <div className="col-md-6" id="entry">
                         <textarea rows="21" cols="45" className="form-control"  placeholder="Enter text" ref="userMessage" onKeyDown={this.generateMarkdown}>{defaultText}</textarea>
                     </div>
-                    <div className="col-md-6" id="rendered" dangerouslySetInnerHTML={this.createMarkup(this.props.newText(defaultText))}>
+                    <div className="col-md-6" id="rendered" dangerouslySetInnerHTML={this.createMarkup(defaultMarkdown)}>
 
                     </div>
                 </div>
@@ -72,17 +76,18 @@ let UserInput = React.createClass({
 
 let AppContainer = React.createClass({
    getInitialState() {
-       return {original: "# H1\n## H2\n### H3\n\n Emphasis, aka italics, with *asterisks* or _underscores_. \n Strong emphasis, aka bold, with **asterisks** or __underscores__.\n" +
-       "Combined emphasis with **asterisks and _underscores_**.\n Strikethrough uses two tildes. ~~Scratch this.~~\n\n"+
+       return {original: "# H1\n## H2\n### H3\n Emphasis, aka italics, with *asterisks* or _underscores_. \n Strong emphasis, aka bold, with **asterisks** or __underscores__.\n" +
+       " Combined emphasis with **asterisks and _underscores_**.\n Strikethrough uses two tildes. ~~Scratch this.~~\n"+
+       "\n Inline-style image: ![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png \"Logo Title Text 1\")\n "+
        " [I'm an inline-style link](https://www.google.com)\n [I'm an inline-style link with title](https://www.google.com \"Google's Homepage\")\n\n" +
        " 1. First ordered list item\n 2. Another item\n ⋅⋅* Unordered sub-list.\n 1. Actual numbers don't matter, just that it's a number\n ⋅⋅1. Ordered sub-list\n 4. And another item.\n\n"+
-        " Inline `code` has `back-ticks around` it."
+        " Inline `code` has `back-ticks around` it.\n ```\n var s = \"JavaScript syntax highlighting\"; \n alert(s);\n```"
        };
    },
 
-   convert(old) {
-       let newValue = md.render(old);
-        return newValue;
+   firstConvert() {
+       let firstRender = md.render(this.state.original);
+        return firstRender;
    } ,
 
    handleConvert(userInput) {
@@ -94,11 +99,11 @@ let AppContainer = React.createClass({
    },
 
    render() {
-       let defaultText = this.state.original;
+       let latestText = this.state.original;
        return (
         <div>
             <Greeting/>
-            <UserInput default={defaultText} onSubmit={this.handleConvert} newText={this.convert}/>
+            <UserInput latest={latestText} onConvert={this.handleConvert} firstMDRender={this.firstConvert}/>
         </div>
 
        )
