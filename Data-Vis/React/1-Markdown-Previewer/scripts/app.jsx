c5834042b02e8cc217cedf805be85e3a75eb13d8
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-04 21:49:38 -0500

    Pass state from container component as props to presentational component for display

--- Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
+++ Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
@@ -12,11 +12,12 @@ let Greeting = React.createClass({
 });
 let UserInput = React.createClass({
     render() {
+        let defText = this.props.default;
         return (
             <div className="display">
                 <div className="row">
                     <div className="col-md-6" id="entry">
-                        <textarea rows="21" cols="45" className="form-control"  placeholder="Enter text"></textarea>
+                        <textarea rows="21" cols="45" className="form-control"  placeholder="Enter text">{defText}</textarea>
                     </div>
                     <div className="col-md-6" id="rendered">
 
@@ -28,16 +29,23 @@ let UserInput = React.createClass({
     }
 });
 
-let App = React.createClass({
+
+
+let AppContainer = React.createClass({
+   getInitialState() {
+       return {original: "# H1\n## H2\n### H3\n#### H4\n##### H5\n###### H6\n\nAlternatively, for H1 and H2, an underline-ish style:\n Alt-H1\n ======\n  Alt-H2\n  ------"};
+   },
+
    render() {
+       let defaultText = this.state.original;
        return (
         <div>
             <Greeting/>
-            <UserInput/>
+            <UserInput default={defaultText}/>
         </div>
 
        )
    } 
 });
 
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<AppContainer/>, document.getElementById("app"));
