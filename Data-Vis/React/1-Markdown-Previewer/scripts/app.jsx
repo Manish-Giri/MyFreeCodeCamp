4be82b94e717e37000dce3dbc6e05ae38da6ed14
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-04 23:00:31 -0500

    Commit first working version of app

--- Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
+++ Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
@@ -1,6 +1,26 @@
 //let UserInput = require('./UserInput')
+//remarkable settings
+// Actual default values
+var md = new Remarkable({
+    html:         true,        // Enable HTML tags in source
+    xhtmlOut:     false,        // Use '/' to close single tags (<br />)
+    breaks:       true,        // Convert '\n' in paragraphs into <br>
+    langPrefix:   'language-',  // CSS language prefix for fenced blocks
+    linkify:      true,        // Autoconvert URL-like text to links
 
-//presentational component - textarea
+    // Enable some language-neutral replacement + quotes beautification
+    typographer:  true,
+
+    // Double + single quotes replacement pairs, when typographer enabled,
+    // and smartquotes on. Set doubles to '«»' for Russian, '„“' for German.
+    quotes: '“”‘’',
+
+    // Highlighter function. Should return escaped HTML,
+    // or '' if the source string is not changed
+    highlight: function (/*str, lang*/) { return ''; }
+});
+
+//presentational component - header
 let Greeting = React.createClass({
    render() {
        return (
@@ -10,16 +30,22 @@ let Greeting = React.createClass({
        )
    }
 });
+
+//presentational component - textarea
 let UserInput = React.createClass({
+
+    createMarkup(newText) {
+        return {__html: newText}
+    },
     render() {
-        let defText = this.props.default;
+        let defaultText = this.props.default;
         return (
             <div className="display">
                 <div className="row">
                     <div className="col-md-6" id="entry">
-                        <textarea rows="21" cols="45" className="form-control"  placeholder="Enter text">{defText}</textarea>
+                        <textarea rows="21" cols="45" className="form-control"  placeholder="Enter text">{defaultText}</textarea>
                     </div>
-                    <div className="col-md-6" id="rendered">
+                    <div className="col-md-6" id="rendered" dangerouslySetInnerHTML={this.createMarkup(this.props.newText(defaultText))}>
 
                     </div>
                 </div>
@@ -36,12 +62,23 @@ let AppContainer = React.createClass({
        return {original: "# H1\n## H2\n### H3\n#### H4\n##### H5\n###### H6\n\nAlternatively, for H1 and H2, an underline-ish style:\n Alt-H1\n ======\n  Alt-H2\n  ------"};
    },
 
+   convert(old) {
+       let newValue = md.render(old);
+        return newValue;
+   } ,
+
+   handleConvert(old) {
+       let oldValue = this.state.original;
+       let converted = md.render(oldValue);
+       this.setState({original: converted});
+   },
+
    render() {
        let defaultText = this.state.original;
        return (
         <div>
             <Greeting/>
-            <UserInput default={defaultText}/>
+            <UserInput default={defaultText} onSubmit={this.handleConvert} newText={this.convert}/>
         </div>
 
        )
