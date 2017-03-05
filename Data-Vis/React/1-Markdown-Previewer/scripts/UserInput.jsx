13a319ccd39a9803565467d36e2730a582519ba1
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-04 20:33:07 -0500

    Commit JSX

---
+++ Data-Vis/React/1-Markdown-Previewer/scripts/UserInput.jsx
@@ -0,0 +1,11 @@
+let UserInput = React.createClass({
+   render() {
+       return (
+           <div>
+               <textarea rows="20" cols="50" className="form-control"  placeholder="Enter text"></textarea>
+           </div>
+       )
+   }
+});
+
+module.exports = UserInput;
--- Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
+++ Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
@@ -1,6 +1,42 @@
+//let UserInput = require('./UserInput')
+
+//presentational component - textarea
+let Greeting = React.createClass({
+   render() {
+       return (
+           <div className="header">
+               <h1>Markdown Previewer</h1>
+           </div>
+       )
+   }
+});
+let UserInput = React.createClass({
+    render() {
+        return (
+            <div className="display">
+                <div className="row">
+                    <div className="col-md-6" id="entry">
+                        <textarea rows="21" cols="45" className="form-control"  placeholder="Enter text"></textarea>
+                    </div>
+                    <div className="col-md-6" id="rendered">
+
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+});
+
 let App = React.createClass({
    render() {
-       return <h1>Hello World</h1>
+       return (
+        <div>
+            <Greeting/>
+            <UserInput/>
+        </div>
+
+       )
    } 
 });
 
