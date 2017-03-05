6f49e65ec8e541ad353d2a4902288367555326ac
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-05 01:28:52 -0500

    Commit latest code

--- Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
+++ Data-Vis/React/1-Markdown-Previewer/scripts/app.jsx
@@ -37,13 +37,26 @@ let UserInput = React.createClass({
     createMarkup(newText) {
         return {__html: newText}
     },
+
+    generateMarkdown(event) {
+        let messageRef = this.refs.userMessage;
+        let messageVal = messageRef.value;
+        event.which = event.which || event.keyCode;
+        if(event.which == 13) {
+            //enter clicked
+            console.log(messageVal);
+            //alert("Enter pressed");
+            //call parent component with new value
+
+        }
+    },
     render() {
         let defaultText = this.props.default;
         return (
             <div className="display">
                 <div className="row">
                     <div className="col-md-6" id="entry">
-                        <textarea rows="21" cols="45" className="form-control"  placeholder="Enter text">{defaultText}</textarea>
+                        <textarea rows="21" cols="45" className="form-control"  placeholder="Enter text" ref="userMessage" onKeyDown={this.generateMarkdown}>{defaultText}</textarea>
                     </div>
                     <div className="col-md-6" id="rendered" dangerouslySetInnerHTML={this.createMarkup(this.props.newText(defaultText))}>
 
@@ -59,7 +72,12 @@ let UserInput = React.createClass({
 
 let AppContainer = React.createClass({
    getInitialState() {
-       return {original: "# H1\n## H2\n### H3\n#### H4\n##### H5\n###### H6\n\nAlternatively, for H1 and H2, an underline-ish style:\n Alt-H1\n ======\n  Alt-H2\n  ------"};
+       return {original: "# H1\n## H2\n### H3\n\n Emphasis, aka italics, with *asterisks* or _underscores_. \n Strong emphasis, aka bold, with **asterisks** or __underscores__.\n" +
+       "Combined emphasis with **asterisks and _underscores_**.\n Strikethrough uses two tildes. ~~Scratch this.~~\n\n"+
+       " [I'm an inline-style link](https://www.google.com)\n [I'm an inline-style link with title](https://www.google.com \"Google's Homepage\")\n\n" +
+       " 1. First ordered list item\n 2. Another item\n ⋅⋅* Unordered sub-list.\n 1. Actual numbers don't matter, just that it's a number\n ⋅⋅1. Ordered sub-list\n 4. And another item.\n\n"+
+        " Inline `code` has `back-ticks around` it."
+       };
    },
 
    convert(old) {
@@ -67,10 +85,12 @@ let AppContainer = React.createClass({
         return newValue;
    } ,
 
-   handleConvert(old) {
-       let oldValue = this.state.original;
-       let converted = md.render(oldValue);
-       this.setState({original: converted});
+   handleConvert(userInput) {
+       if(userInput.length > 0) {
+           let converted = md.render(userInput);
+           this.setState({original: converted});
+       }
+       //let oldValue = this.state.original;
    },
 
    render() {
