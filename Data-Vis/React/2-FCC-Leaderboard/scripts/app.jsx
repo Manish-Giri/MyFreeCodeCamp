30a8d67aa549e3ee2ba9e5763c520e402533c293
MANISH-GIRI <manish.giri.me@gmail.com>
2017-03-11 00:12:44 -0500

    Commit latest code

--- Data-Vis/React/2-FCC-Leaderboard/scripts/app.jsx
+++ Data-Vis/React/2-FCC-Leaderboard/scripts/app.jsx
@@ -10,6 +10,47 @@
 
 
 
+class LeaderBoardTable extends React.Component {
+
+    render() {
+        return (
+            <div className="table-holder">
+                <table className="bordered">
+                    <thead>
+                    <tr>
+                        <th data-field="id">Name</th>
+                        <th data-field="name">Item Name</th>
+                        <th data-field="price">Item Price</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    <tr>
+                        <td>Alvin</td>
+                        <td>Eclair</td>
+                        <td>$0.87</td>
+                    </tr>
+                    <tr>
+                        <td>Alan</td>
+                        <td>Jellybean</td>
+                        <td>$3.76</td>
+                    </tr>
+                    <tr>
+                        <td>Jonathan</td>
+                        <td>Lollipop</td>
+                        <td>$7.00</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    }
+
+}
+
+
+
 class FilterableLeaderBoard extends React.Component {
     constructor(props) {
         super(props);
@@ -27,7 +68,8 @@ class FilterableLeaderBoard extends React.Component {
 
 
         return (
-            <div>
+            <div className="main-container light-green darken-4">
+                <LeaderBoardTable />
             </div>
         )
     }
