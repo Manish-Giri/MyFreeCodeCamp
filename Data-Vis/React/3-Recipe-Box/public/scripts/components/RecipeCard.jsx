bbdd7317e480a249116bca28c680b4ef023b1ca1
MANISH-GIRI <manish.giri.me@gmail.com>
2017-04-02 04:27:10 -0400

    Style the recipe card - add delete button to main card, style title, add edit button to card reveal, style header and ingredients

--- Data-Vis/React/3-Recipe-Box/public/scripts/components/RecipeCard.jsx
+++ Data-Vis/React/3-Recipe-Box/public/scripts/components/RecipeCard.jsx
@@ -1,11 +1,24 @@
 var React = require('react');
 
 let styles = {
+  cardContent: {
+    title: {
+        color: "#212121",
+        fontFamily:"'Playfair Display SC', serif",
+        fontSize: 34,
+        marginTop: -5
+    },
+
+    deleteButton: {
+
+    }
+  },
+
   cardReveal: {
       header: {
           color: "#5d4037",
           fontFamily:"'Playfair Display SC', serif",
-          padding: 2,
+          padding: 5,
           margin: 10
       },
 
@@ -24,24 +37,28 @@ let styles = {
 class RecipeCard extends React.Component {
     render() {
         return (
-            <div className="card small hoverable">
+            <div className="card medium hoverable">
                 <div className="card-image waves-effect waves-block waves-light">
                     <img className="activator" src="https://search.chow.com/thumbnail/320/0/www.chowstatic.com/s/recipe_placeholder_main_img-710dbe7756144f55f01c42cc4892e6de.jpg"/>
                 </div>
                 <div className="card-content">
-                    <span className="card-title activator grey-text text-darken-2">Card Title<i className="material-icons orange-text text-darken-4 right">more_vert</i></span>
-                    <p><a href="#">This is a link</a></p>
+                    <span className="card-title activator grey-text text-darken-2"><i className="material-icons brown-text text-darken-4 right">more_vert</i></span>
+                    <h1 style={styles.cardContent.title}>Sphagetti</h1>
+                    {/*<p><a href="#">This is a link</a></p>*/}
+                    <a className="btn-floating btn-large waves-effect waves-light red right"><i className="material-icons right">delete</i></a>
                 </div>
                 <div className="card-reveal">
                     <span className="card-title grey-text text-darken-4"><i className="material-icons right">close</i></span>
                     <h4 style={styles.cardReveal.header}>Sphagetti</h4>
                     {/*<h5>Ingredients</h5>*/}
                     <div className="collection">
-                        <a href="#!" className="collection-item">Alvin</a>
-                        <a href="#!" className="collection-item active">Alvin</a>
-                        <a href="#!" className="collection-item">Alvin</a>
-                        <a href="#!" className="collection-item">Alvin</a>
+                        <a href="#!" className="collection-item"><span className="badge">1</span>Alvin</a>
+                        <a href="#!" className="collection-item active"><span className="badge">1</span>Alvin</a>
+                        <a href="#!" className="collection-item"><span className="badge">1</span>Alvin</a>
+                        <a href="#!" className="collection-item"><span className="badge">1</span>Alvin</a>
+
                     </div>
+                    <a className="btn-floating btn-large waves-effect waves-light cyan darken-4 right"><i className="material-icons right">mode_edit</i></a>
                 </div>
             </div>
         )
