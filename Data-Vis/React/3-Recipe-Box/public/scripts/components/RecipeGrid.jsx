1d5a0484e80b50ab1abbee0082d8bcec2d57bd96
MANISH-GIRI <manish.giri.me@gmail.com>
2017-04-01 23:14:31 -0400

    Create RecipeCard component to hold each Recipe, RecipeGrid contains RecipeCard

--- Data-Vis/React/3-Recipe-Box/public/scripts/components/RecipeGrid.jsx
+++ Data-Vis/React/3-Recipe-Box/public/scripts/components/RecipeGrid.jsx
@@ -1,18 +1,19 @@
 var React = require('react');
+var RecipeCard = require('./RecipeCard');
 
 class RecipeGrid extends React.Component {
     render() {
         let style = {
             backgroundColor: "white",
             height: "50px",
-          
+
         };
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col s12 m4 l4" style={style}><p>s12 m4</p></div>
-                    <div className="col s12 m4 l4" style={style}><p>s12 m4</p></div>
-                    <div className="col s12 m4 l4" style={style}><p>s12 m4</p></div>
+                    <div className="col s12 m4 l4"><p><RecipeCard/></p></div>
+                    <div className="col s12 m4 l4"><p><RecipeCard/></p></div>
+                    <div className="col s12 m4 l4"><p><RecipeCard/></p></div>
                 </div>
             </div>
         )
