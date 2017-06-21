1f0d41e93f62eb1cddf18032369b5b45f7e530cd
MANISH-GIRI <manish.giri.me@gmail.com>
2017-06-21 18:44:44 -0400

    Setup Project for Bar Chart

---
+++ Data-Vis/D3/1-Bar-Chart/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by manishgiri on 6/21/17.
+ */
+
+
+d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json", function (result) {
+    console.log(result.data);
+    let data = result.data;
+    
+})
