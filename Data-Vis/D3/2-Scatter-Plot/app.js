335d850c31ce3f4aed416cb7624153b1bff0a50e
MANISH-GIRI <manish.giri.me@gmail.com>
2017-06-28 16:52:01 -0400

    Create Axes labels, commit final

--- Data-Vis/D3/2-Scatter-Plot/app.js
+++ Data-Vis/D3/2-Scatter-Plot/app.js
@@ -9,7 +9,7 @@ const margin = {top: 10, right: 60, bottom: 60, left: 60};
 // SVG values
 let width = 1060 - margin.left - margin.right;
 let height = 500 - margin.top - margin.bottom;
-
+let padding = 100;
 
 // create div for tooltip
 let div = d3.select("body")
@@ -31,13 +31,6 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     "use strict";
     let data = result;
     console.log(data);
-/*
-    console.log(d3.min(data.map(row => d3.timeParse("%M:%S")(row.Time))));
-    console.log(d3.max(data.map(row => d3.timeParse("%M:%S")(row.Time))));
-
-    console.log(d3.max(data.map(d => d.Place)));*/
-
-    console.log(d3.extent(data, d => d.Place));
 
     // create scales
     let xScale = d3.scaleTime()
@@ -45,14 +38,16 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     .range([0, width])
         .nice();
 
+    // use for axis
     let yScale = d3.scaleLinear()
         .domain(d3.extent(data, row => row.Place))
         .range([0, height])
         .nice();
 
+    // use for plot
     let yScaleDup = d3.scaleLinear()
         .domain(d3.extent(data, row => row.Place))
-        .range([0, height-4])
+        .range([0, height-3])
         .nice();
 
     // create scatter plot
@@ -63,14 +58,14 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
         .attr("cx", d => xScale(d3.timeParse("%M:%S")(d.Time)))
         .attr("cy", d => yScaleDup(+d.Place))
         .attr("r", 6)
-        .attr("fill", d => d.Doping ? "#ef5350" : "#8bc34a")
+        .attr("fill", d => d.Doping ? "#f44336" : "#4caf50")
         .attr("stroke", "black")
         // create tooltips
         .on("mouseover", d => {
             div.transition()
                 .duration(300)
                 .style("opacity", 0.9)
-            div.html(`<b>${d.Name}-${d.Nationality}</b><br> Time: ${d.Time}, Year: ${d.Year} <br> ${d.Doping}`)
+            div.html(`<span class="about">${d.Name} - ${d.Nationality}</span><br> Time: ${d.Time}, Year: ${d.Year} <br> ${d.Doping}`)
                 .style("left", (d3.event.pageX) - 60 + "px")
                 .style("top", (d3.event.pageY) - 130 + "px")
         })
@@ -93,6 +88,22 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
         .attr("class", "axisStyle" )
         .call(d3.axisLeft(yScale));
 
-    // create tooltips
+    // create axes labels
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", 0 - (height/2))
+        .attr("y", 0 - (margin.left) + 10)
+        .attr("dy", "1em")
+        .attr("class", "label")
+        .style("color", "white")
+        .style("text-anchor", "middle")
+        .text("Rank")
+
+    svg.append("text")
+        .attr("transform", "translate(" + (width/2) + "," + (height + 50) + ")" )
+        .style("text-anchor", "middle")
+        .attr("class", "label")
+        .text("Time")
+
 
 });
