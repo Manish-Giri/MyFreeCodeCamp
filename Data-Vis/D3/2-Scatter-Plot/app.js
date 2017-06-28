c031617a4a699394f697029a2dc7daeee06c7a99
MANISH-GIRI <manish.giri.me@gmail.com>
2017-06-28 03:41:30 -0400

    Create Y axis and style both axes

--- Data-Vis/D3/2-Scatter-Plot/app.js
+++ Data-Vis/D3/2-Scatter-Plot/app.js
@@ -7,7 +7,7 @@
 const margin = {top: 10, right: 60, bottom: 60, left: 60};
 
 // SVG values
-let width = 960 - margin.left - margin.right;
+let width = 1060 - margin.left - margin.right;
 let height = 500 - margin.top - margin.bottom;
 
 // create SVG element
@@ -23,11 +23,13 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     "use strict";
     let data = result;
     console.log(data);
-
+/*
     console.log(d3.min(data.map(row => d3.timeParse("%M:%S")(row.Time))));
     console.log(d3.max(data.map(row => d3.timeParse("%M:%S")(row.Time))));
 
-    console.log(d3.max(data.map(d => d.Place)));
+    console.log(d3.max(data.map(d => d.Place)));*/
+
+    console.log(d3.extent(data, d => d.Place));
 
     // create scales
     let xScale = d3.scaleTime()
@@ -40,13 +42,18 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
         .range([0, height])
         .nice();
 
+    let yScaleDup = d3.scaleLinear()
+        .domain(d3.extent(data, row => row.Place))
+        .range([0, height-4])
+        .nice();
+
     // create scatter plot
     svg.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
         .attr("cx", d => xScale(d3.timeParse("%M:%S")(d.Time)))
-    .attr("cy", d => yScale(+d.Place))
+    .attr("cy", d => yScaleDup(+d.Place))
     .attr("r", 6)
         .attr("fill", d => d.Doping ? "#ef5350" : "#8bc34a")
     .attr("stroke", "black")
@@ -54,8 +61,12 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     // create axes
     // x axis
     svg.append("g")
-        .attr("class", "axisBottom")
+        .attr("class", "axisStyle")
         .attr("transform", "translate(0," + (height+2) + ")")
         .call(d3.axisBottom(xScale).tickFormat(d3.timeFormat("%M:%S")));
 
+    // y axis
+    svg.append("g")
+        .attr("class", "axisStyle" )
+        .call(d3.axisLeft(yScale));
 });
