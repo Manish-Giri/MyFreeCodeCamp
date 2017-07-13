70f66434f04b24ebd914b55b3d97a0d52dd73f9d
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-13 01:19:24 -0400

    Setup initial state of nodes and edges with result from Axios.get

--- Data-Vis/D3/4-Force-Graph/app.jsx
+++ Data-Vis/D3/4-Force-Graph/app.jsx
@@ -3,13 +3,59 @@ const Header = () => {
 };
 
 class Chart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nodes: [],
+            edges: []
+        }
+    }
 
+    componentDidMount() {
+        axios.get('https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json')
+            .then( response => {
+                // console.log(response);
+                // set up nodes
+                let vertices = response.data.nodes.map((object, index) => {
+                    return {
+                        id: index,
+                        label: object.country,
+                        flag: `https://github.com/hjnilsson/country-flags/raw/master/png100px/${object.code}.png`
+                    }
+                });
+                // console.log(n);
+                // setup edges
+                let links = response.data.links.map(link => {
+                    return {
+                        from: link.source,
+                        to: link.target
+                    }
+                });
+                this.setState({nodes: vertices, edges: links});
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        let graph = {
+            nodes: this.state.nodes,
+            edges: this.state.edges
+        };
+
+        let options = {
+            
+        }
+        return <h3 nodes={this.state.nodes}>Chart here</h3>
+    }
 }
 
 const App = () => {
         return (
             <div>
                 <Header/>
+                <Chart/>
             </div>
         )
 }
