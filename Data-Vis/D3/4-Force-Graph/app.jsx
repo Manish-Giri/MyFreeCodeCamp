5cedc671500068119d554ea58571e85fef597bd0
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-27 02:05:45 -0400

    Commit final code

--- Data-Vis/D3/4-Force-Graph/app.jsx
+++ Data-Vis/D3/4-Force-Graph/app.jsx
@@ -12,39 +12,58 @@ class Chart extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json')
-            .then( response => {
-                // console.log(response);
-                // set up nodes
-                let vertices = response.data.nodes.map((object, index) => {
-                    return {
-                        id: index,
-                        label: object.country,
-                        image: `https://github.com/hjnilsson/country-flags/raw/master/png100px/${object.code}.png`,
-                        shape: "circularImage",
-                        size: 15,
-                        color: {
-                            border: "#222"
+        axios.get('https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json').then(response => {
+            // console.log(response);
+            // set up nodes
+            let vertices = response.data.nodes.map((object, index) => {
+                return {
+                    id: index,
+                    label: object.country,
+                    image: `https://github.com/hjnilsson/country-flags/raw/master/png100px/${object.code}.png`,
+                    borderWidth: 2,
+                    borderWidthSelected: 5,
+                    brokenImage: 'https://raw.githubusercontent.com/hjnilsson/country-flags/master/png100px/fr.png',
+                    chosen: {
+                        node: function (values, id, selected, hovering) {
+                            // values.size = 26;
+                            values.borderColor = "red";
+                            values.shadow = true;
                         },
-                        font: {
-                            color: "#eeeeee",
-                            face: "Open Sans', sans-serif"
+                        label: function (values, id, selected, hovering) {
+                            values.size = 18;
+                            values.mod = "bold"
                         }
+                    },
+                    color: {
+                        border: "#222",
+                        hover: {
+                            border: "#2B7CE9"
+                        }
+                    },
+                    font: {
+                        color: "#eeeeee",
+                        face: "Open Sans', sans-serif",
+                        size: 18,
+                        bold: "18px solid '#eee'"
+                    },
+                    shape: "circularImage",
+                    size: 15,
+                    shapeProperties: {
+                        interpolation: true,
+                        size: 26
                     }
-                });
-                // console.log(n);
-                // setup edges
-                let links = response.data.links.map(link => {
-                    return {
-                        from: link.source,
-                        to: link.target
-                    }
-                });
-                this.setState({nodes: vertices, edges: links});
-            })
-            .catch(function (error) {
-                console.log(error);
+
+                }
             });
+            // console.log(n);
+            // setup edges
+            let links = response.data.links.map(link => {
+                return {from: link.source, to: link.target}
+            });
+            this.setState({nodes: vertices, edges: links});
+        }).catch(function(error) {
+            console.log(error);
+        });
     }
 
     render() {
@@ -56,25 +75,30 @@ class Chart extends React.Component {
         let options = {
             layout: {
                 hierarchical: false,
-                improvedLayout:false
+                improvedLayout: false
             },
             height: '100%',
             width: '100%',
             edges: {
                 color: "#efefef",
-                arrows: { to: { enabled: false } },
-            },
-            nodes: {
-                shapeProperties: {
-                    interpolation: false    // 'true' for intensive zooming
-                }
+                arrows: {
+                    to: {
+                        enabled: false
+                    }
+                },
+                chosen: {
+                  edge: function (values, id, selected, hovering) {
+                      values.toArrow = true;
+
+                  }
+                },
             }
+
         };
 
         let styles = {
-            width: "90%",
+            width: "98%",
             height: "100%",
-            border: "1px solid red",
             margin: "5px auto"
         };
 
@@ -88,13 +112,13 @@ class Chart extends React.Component {
 }
 
 const App = () => {
-        return (
-            <div>
-                <Header/>
-                <Chart/>
-            </div>
-        )
+    return (
+        <div>
+            <Header/>
+            <Chart/>
+        </div>
+    )
 }
 
-
-ReactDOM.render(<App />, document.getElementById("main"));
+ReactDOM.render(
+    <App/>, document.getElementById("main"));
