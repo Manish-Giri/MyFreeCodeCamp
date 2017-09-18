820b05f693ea84446757c442ac236fd589d4d96e
MANISH-GIRI <manish.giri.me@gmail.com>
2017-09-17 23:51:15 -0400

    Commit JS file

---
+++ Data-Vis/D3/5-Globe-Map/app.js
@@ -0,0 +1,11 @@
+const width = 1000;
+const height = 600;
+
+const worldMap = "https://github.com/johan/world.geo.json";
+const meteorData = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json";
+
+
+    
+
+
+
